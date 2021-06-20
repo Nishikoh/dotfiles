a6ca68a95c328fa3de71332b25a45805d1dfb604
Koki Nishihara <>
2021-06-20 17:23:26 +0000

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -exu
-echo $1
 
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-if [ "$1" = "linux" ]; then
-    PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
-fi
+PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
 brew bundle
