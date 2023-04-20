fe4112855a3394ce57eeb7989811dc2ba533f0c1
Koki Nishihara <snkg2450@gmail.com>
2023-04-20 21:47:07 +0900

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -exu
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install xcp
+
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
 brew update
@@ -8,6 +11,3 @@ brew bundle
 pipx ensurepath
 pipx completions
 pipx install poetry
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install xcp
