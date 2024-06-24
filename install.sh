a0d332c2bbb1fb9d69a79d6c8336147968650c2c
Koki Nishihara <snkg2450@gmail.com>
2024-06-25 02:51:28 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,12 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 cargo install xcp
+cargo install typos
 
 which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
 brew update
 brew bundle
-
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-cargo install xcp
-cargo install typos
-
 
 curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
 
