cda69a72214b2df37d309523aa4f201faba0c72f
Koki Nishihara <snkg2450@gmail.com>
2024-11-07 03:23:14 +0900

    Update install.sh
    use devbox

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -exu
 
+curl -fsSL https://get.jetify.com/devbox | bash
+eval "$(devbox global shellenv)"
+# TODO latest config
+devbox global pull https://gist.githubusercontent.com/Nishikoh/c58e4963b7abac7d06574360a1cd653b/raw/5f61b0294e6a2481e4078f43b3a70e21b571268d/devbox.json
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-cargo install cargo-binstall
-cargo install cpz
-cargo install rmz
-cargo binstall fcp
+#cargo install cargo-binstall
+#cargo binstall cpz
+#cargo binstall rmz
 
-cargo install xcp
-#cargo install typos
+#cargo binstall xcp
 
-which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
-brew update
-brew bundle
+#which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#PATH="/home/linuxbrew/.linuxbrew/bin:${PATH}"
+#brew update
+#brew bundle
 
-curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
 
-echo "export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin/" >> ~/.bashrc
+#echo "export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin/" >> ~/.bashrc
