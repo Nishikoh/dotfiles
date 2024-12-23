51c49f646043c231dbe76e924d8295c4b8a68589
Koki Nishihara <snkg2450@gmail.com>
2024-12-24 05:35:14 +0900

    initialize devbox shell environment in last.zsh
    前のスクリプトだと補完が効かなくなる

--- zsh/99last.zsh
+++ zsh/99last.zsh
@@ -1,5 +1,2 @@
-# このファイルがあるディレクトリパス
-ZSH_LAST_PATH=$(dirname "$0")
-
-# echo "debug: $ZSH_LAST_PATH"
-source "$ZSH_LAST_PATH"/02completion.zsh
+eval "$(devbox global shellenv)"
+eval "$(devbox shellenv)"
