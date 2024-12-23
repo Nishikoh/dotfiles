8fb0a577c1c12de403988bbbb8a55a0c377e4442
Koki Nishihara <snkg2450@gmail.com>
2024-12-24 03:33:27 +0900

    refactor completion setup in zsh configuration to simplify autoload process
    消した部分が悪さをしてargcの補完を聞かないようにしていた

--- zsh/02completion.zsh
+++ zsh/02completion.zsh
@@ -3,24 +3,6 @@
 # -----------------------------
 plugins=(docker docker-compose kubectl)
 
-# brew 　の場合
-fpath=($(brew --prefix)/share/zsh/site-functions "$fpath")
-
-# brew じゃない
-fpath=(~/.zsh/completion "$fpath")
-
-#zsh-completions 補完
-if [ -e /usr/local/share/zsh-completions ]; then
-	fpath=(/usr/local/share/zsh-completions "$fpath")
-fi
-
-if type brew &>/dev/null; then
-	FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
-
-	autoload -Uz compinit
-	compinit
-fi
-
 # 自動補完を有効にする
 autoload -Uz compinit
 compinit -u
