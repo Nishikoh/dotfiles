857ebbfd7ed5755dfb6efed3d429654b52e3a3fc
Koki Nishihara <koki.nishihara@craftsman-software.com>
2025-01-31 10:38:20 +0900

    update PATH variable to include dotfiles and Krew binaries

--- zsh/10path.zsh
+++ zsh/10path.zsh
@@ -4,6 +4,8 @@
 # パスを追加したい場合
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/setup/bin:$PATH"
+export PATH="$HOME/setup/dotfiles/bin:$PATH"
 
 . "$HOME/.cargo/env"
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
