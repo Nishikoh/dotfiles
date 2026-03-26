555fb5edf1bd002c3c7e3c1ae00f04a986098f50
Koki Nishihara <snkg2450@gmail.com>
2026-03-27 04:39:09 +0900

    zsh: Nixプロファイル初期化スクリプトを読み込む設定を追加
    zsh/02completion.zsh に Nix インストーラが追加する ~/.nix-profile/etc/profile.d/nix.sh を読み込む行を追加。これにより Nix プロファイルの PATH や環境変数が zsh セッションで正しく反映されるようになります。

--- zsh/02completion.zsh
+++ zsh/02completion.zsh
@@ -64,3 +64,5 @@ source <(fzf --zsh)
 
 # bun completions
 [ -s "~/.bun/_bun" ] && source "~/.bun/_bun"
+
+if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
