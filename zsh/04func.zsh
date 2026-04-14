b59beeab8e4bc79b3cf3170fff46bd28b52d6ae0
Koki Nishihara <snkg2450@gmail.com>
2026-04-14 19:16:46 +0900

    zsh: yazi 用の y() ヘルパー関数を追加
    yazi のクイックスタートに基づく簡易ランチャーを追加。
    mktemp で一時ファイルを作成し、yazi を --cwd-file で呼び出して選択された作業ディレクトリを読み取り、
    現在のディレクトリと異なれば移動する。最後に一時ファイルを削除してクリーンアップ。

--- zsh/04func.zsh
+++ zsh/04func.zsh
@@ -61,6 +61,15 @@ lh() {
 	lefthook "$@"
 }
 
+# https://yazi-rs.github.io/docs/quick-start
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
 # fzf-diff() {
 #   preview="git diff $@ --color=always -- {-1} | delta"
 #   git diff $@ --name-only | fzf -m --ansi --preview-window='down:85%' --preview $preview
